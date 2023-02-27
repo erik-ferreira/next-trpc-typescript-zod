cb13afb1a89095098dea30e80d81e42467c01954
erik-ferreira <erikdesouzagf@gmail.com>
2023-02-27 15:10:49 -0300

    add tRPC in project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { trpc } from "../utils/trpc";
 
-import '../styles/global.css'
+import "../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default trpc.withTRPC(MyApp);
